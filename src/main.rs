e0912ae66a2f3d6b3ac115e0291f133101906abb
Ricardo <ricarditomontserrat@gmail.com>
2024-06-16 18:24:00 +0200

    Shoots isaacs, shitty components structure

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,43 @@
 use bevy::{app::AppExit, prelude::*};
 
+#[derive(Component)]
+struct Player {
+    speed: f32,
+}
+
+#[derive(Component)]
+struct Bullet {
+    speed: f32,
+}
+
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteBundle {
+
+    let player = Player { speed: 5. };
+    let player_sprite = SpriteBundle {
         texture: asset_server.load("sakuya.png"),
         transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(0.2)),
         ..default()
-    });
+    };
+    commands.spawn((player, player_sprite));
+
+    for i in 0..10 {
+        let bullet = Bullet { speed: 5. };
+        let bullet_sprite = SpriteBundle {
+            texture: asset_server.load("isaac.png"),
+            transform: Transform::from_xyz(0., 200. * i as f32, 0.).with_scale(Vec3::splat(0.2)),
+            ..default()
+        };
+        commands.spawn((bullet, bullet_sprite));
+    }
 }
 
-fn update_player(mut query: Query<(&mut Transform, &Sprite)>, keys: Res<ButtonInput<KeyCode>>) {
-    let speed = 5.0;
-
-    for (mut transform, _) in query.iter_mut() {
+fn update_player(mut query: Query<(&Player, &mut Transform)>, keys: Res<ButtonInput<KeyCode>>) {
+    for (player, mut transform) in &mut query {
         let mut if_press_move = |key: KeyCode, dir: (f32, f32)| {
             if keys.pressed(key) {
-                transform.translation.x += dir.0 * speed;
-                transform.translation.y += dir.1 * speed;
+                transform.translation.x += dir.0 * player.speed;
+                transform.translation.y += dir.1 * player.speed;
             }
         };
 
@@ -24,6 +45,12 @@ fn update_player(mut query: Query<(&mut Transform, &Sprite)>, keys: Res<ButtonIn
         if_press_move(KeyCode::KeyA, (-1., 0.));
         if_press_move(KeyCode::KeyS, (0., -1.));
         if_press_move(KeyCode::KeyD, (1., 0.));
+    }
+}
+
+fn update_bullets(mut query: Query<(&Bullet, &mut Transform)>) {
+    for (bullet, mut transform) in &mut query {
+        transform.translation.y += bullet.speed;
     }
 }
 
@@ -37,6 +64,6 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, startup)
-        .add_systems(Update, (update_player, escape_game))
+        .add_systems(Update, (update_player, escape_game, update_bullets))
         .run();
 }
