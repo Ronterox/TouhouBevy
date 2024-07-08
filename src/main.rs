274c783c226e16b1cd59f5115107fc3884011dbf
Ricardo <ricarditomontserrat@gmail.com>
2024-07-08 17:59:17 +0200

    Removed all UI, added enemy movement patterns

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,11 @@ struct Player {
 }
 
 #[derive(Component)]
-struct Enemy {}
+struct Enemy {
+    velocities: Vec<f32>,
+    vel_index: usize,
+    speed: f32,
+}
 
 #[derive(Component)]
 struct Bullet {
@@ -15,6 +19,9 @@ struct Bullet {
 
 #[derive(Resource)]
 struct BulletTimer(Timer);
+
+#[derive(Resource)]
+struct EnemyTimer(Timer);
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
@@ -27,7 +34,11 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     };
     commands.spawn((player, player_sprite));
 
-    let enemy = Enemy {};
+    let enemy = Enemy {
+        velocities: vec![-1., 0., 1., 0., 1., 0., -1., 0.],
+        speed: 5.,
+        vel_index: 0,
+    };
     let enemy_sprite = SpriteBundle {
         texture: asset_server.load("sakuya.png"),
         transform: Transform::from_xyz(0., 200., 0.).with_scale(Vec3::splat(0.15)),
@@ -39,47 +50,12 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         let bullet = Bullet { speed: 20. };
         let bullet_sprite = SpriteBundle {
             texture: asset_server.load("isaac.png"),
-            transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(0.1)),
+            transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(0.05)),
             visibility: Visibility::Hidden,
             ..default()
         };
         commands.spawn((bullet, bullet_sprite, player));
     }
-
-    commands
-        .spawn(NodeBundle {
-            style: Style {
-                width: Val::Percent(80.),
-                height: Val::Percent(80.),
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
-                margin: UiRect::all(Val::Auto),
-                ..default()
-            },
-            background_color: Color::WHITE.into(),
-            ..default()
-        })
-        .with_children(|panel| {
-            panel
-                .spawn(ButtonBundle {
-                    background_color: Color::BLACK.into(),
-                    style: Style {
-                        padding: UiRect::all(Val::Px(10.)),
-                        ..default()
-                    },
-                    ..default()
-                })
-                .with_children(|button| {
-                    button.spawn(TextBundle::from_section(
-                        "Increment Speed",
-                        TextStyle {
-                            font_size: 40.0,
-                            color: Color::WHITE,
-                            ..default()
-                        },
-                    ));
-                });
-        });
 }
 
 fn move_by(transform: &mut Mut<Transform>, dir: (f32, f32), speed: f32) {
@@ -111,8 +87,8 @@ fn update_bullet_shot(
         (&mut Transform, &ViewVisibility, &mut Visibility),
         (With<Player>, With<Bullet>),
     >,
-    time: Res<Time>,
     mut timer: ResMut<BulletTimer>,
+    time: Res<Time>,
 ) {
     if timer.0.tick(time.delta()).just_finished() {
         query_bullets
@@ -125,11 +101,24 @@ fn update_bullet_shot(
     }
 }
 
-fn update_bullets(mut query: Query<(&Bullet, &mut Transform, &ViewVisibility)>) {
+fn update_bullets_position(mut query: Query<(&Bullet, &mut Transform, &ViewVisibility)>) {
     for (bullet, mut transform, visibility) in &mut query {
         if visibility.get() {
             transform.translation.y += bullet.speed;
         }
+    }
+}
+
+fn update_enemy_position(
+    mut query: Query<(&mut Enemy, &mut Transform)>,
+    mut timer: ResMut<EnemyTimer>,
+    time: Res<Time>,
+) {
+    for (mut enemy, mut transform) in &mut query {
+        if timer.0.tick(time.delta()).just_finished() {
+            enemy.vel_index = (enemy.vel_index + 1) % enemy.velocities.len();
+        }
+        transform.translation.x += enemy.velocities[enemy.vel_index] * enemy.speed;
     }
 }
 
@@ -151,33 +140,21 @@ fn change_colors(
         .for_each(|mut sprite| sprite.color = Color::RED);
 }
 
-fn hide_ui(mut query: Query<&mut Visibility, With<Node>>) {
-    for mut visibility in &mut query {
-        *visibility = Visibility::Hidden;
-    }
-}
-
-fn toggle_ui(
-    mut query: Query<(&mut Visibility, &mut ViewVisibility), With<Node>>,
-    keys: Res<ButtonInput<KeyCode>>,
-) {
-    if keys.just_pressed(KeyCode::Space) {
-        for (mut visibility, view_visibility) in &mut query {
-            *visibility = match view_visibility.get() {
-                true => Visibility::Hidden,
-                false => Visibility::Visible,
-            };
-        }
-    }
-}
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(BulletTimer(Timer::from_seconds(0.1, TimerMode::Repeating)))
+        .insert_resource(EnemyTimer(Timer::from_seconds(1.5, TimerMode::Repeating)))
         .add_systems(Startup, startup)
-        .add_systems(PostStartup, (change_colors, hide_ui))
-        .add_systems(PreUpdate, (update_player_position, escape_game, toggle_ui))
-        .add_systems(Update, (update_bullet_shot, update_bullets))
+        .add_systems(PostStartup, change_colors)
+        .add_systems(PreUpdate, (update_player_position, escape_game))
+        .add_systems(
+            Update,
+            (
+                update_bullet_shot,
+                update_bullets_position,
+                update_enemy_position,
+            ),
+        )
         .run();
 }
