8915f3ac1721b0afd375d8696bb42318c1a1b494
Ricardo <ricarditomontserrat@gmail.com>
2024-07-15 02:36:00 +0200

    Generalization of implementations, (all I wanted was to shoot a bullet man)

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,107 @@
 use bevy::{app::AppExit, prelude::*};
 
-#[derive(Component, Clone, Copy)]
-struct Player {
+#[derive(Component)]
+struct PlayerData {
     speed: f32,
+    shot_timer: Timer,
 }
 
-#[derive(Component)]
-struct Enemy {
+#[derive(Component, Clone)]
+struct EnemyData {
     velocities: Vec<f32>,
     speed: f32,
+    // shot_timer: Timer,
+    move_timer: Timer,
 }
 
 #[derive(Component)]
-struct Bullet {
+struct BulletData {
     speed: f32,
 }
 
-#[derive(Resource)]
-struct ShotTimer(Timer);
+#[derive(Bundle)]
+struct BodyData<T: Component> {
+    data: T,
+    sprite: SpriteBundle,
+}
 
-#[derive(Resource)]
-struct EnemyTimer(Timer);
+type Player = BodyData<PlayerData>;
+type Enemy = BodyData<EnemyData>;
+type Bullet = BodyData<BulletData>;
+
+impl Default for Player {
+    fn default() -> Self {
+        Self {
+            data: PlayerData {
+                speed: 10.0,
+                shot_timer: Timer::from_seconds(0.1, TimerMode::Repeating),
+            },
+            sprite: SpriteBundle {
+                transform: Transform::from_xyz(0., -200., 0.).with_scale(Vec3::splat(0.12)),
+                ..default()
+            },
+        }
+    }
+}
+
+impl Default for Enemy {
+    fn default() -> Self {
+        Self {
+            data: EnemyData {
+                velocities: vec![-1., 0., 1., 0., 1., 0., -1., 0.],
+                speed: 5.,
+                // shot_timer: Timer::from_seconds(0.5, TimerMode::Repeating),
+                move_timer: Timer::from_seconds(1.5, TimerMode::Repeating),
+            },
+            sprite: SpriteBundle {
+                transform: Transform::from_xyz(0., 200., 0.).with_scale(Vec3::splat(0.2)),
+                ..default()
+            },
+        }
+    }
+}
+
+impl Default for Bullet {
+    fn default() -> Self {
+        Self {
+            data: BulletData { speed: 20. },
+            sprite: SpriteBundle {
+                transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(0.05)),
+                visibility: Visibility::Hidden,
+                ..default()
+            },
+        }
+    }
+}
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
-    let player = Player { speed: 10. };
-    let player_sprite = SpriteBundle {
-        texture: asset_server.load("sakuya.png"),
-        transform: Transform::from_xyz(0., -200., 0.).with_scale(Vec3::splat(0.2)),
-        ..default()
-    };
-    commands.spawn((player, player_sprite));
-
-    let enemy = Enemy {
-        velocities: vec![-1., 0., 1., 0., 1., 0., -1., 0.],
-        speed: 5.,
-    };
-    let enemy_sprite = SpriteBundle {
-        texture: asset_server.load("sakuya.png"),
-        transform: Transform::from_xyz(0., 200., 0.).with_scale(Vec3::splat(0.15)),
-        ..default()
-    };
-    commands.spawn((enemy, enemy_sprite));
+    let mut player = Player::default();
+    player.sprite.texture = asset_server.load("sakuya.png");
+    commands.spawn(player);
 
     for _ in 0..5 {
-        let bullet = Bullet { speed: 20. };
-        let bullet_sprite = SpriteBundle {
-            texture: asset_server.load("isaac.png"),
-            transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(0.05)),
-            visibility: Visibility::Hidden,
-            ..default()
-        };
-        commands.spawn((bullet, bullet_sprite, player));
+        let mut bullet = Bullet::default();
+        bullet.sprite.texture = asset_server.load("isaac.png");
+        commands.spawn(bullet);
     }
+
+    let mut enemy = Enemy::default();
+    enemy.sprite.texture = asset_server.load("sakuya.png");
+    commands.spawn(enemy);
 }
 
-fn move_by(transform: &mut Mut<Transform>, dir: (f32, f32), speed: f32) {
+fn move_by(transform: &mut Transform, dir: (f32, f32), speed: f32) {
     transform.translation.x += dir.0 * speed;
     transform.translation.y += dir.1 * speed;
 }
 
 fn update_player_position(
-    mut query_player: Query<(&Player, &mut Transform), Without<Bullet>>,
+    mut player_pos: Query<(&mut Transform, &PlayerData)>,
     keys: Res<ButtonInput<KeyCode>>,
 ) {
-    let (player, mut player_transform) = query_player.single_mut();
+    let (mut player_transform, player) = player_pos.single_mut();
 
     let mut if_press_move = |key: KeyCode, dir: (f32, f32)| {
         if keys.pressed(key) {
@@ -79,27 +115,27 @@ fn update_player_position(
     if_press_move(KeyCode::KeyD, (1., 0.));
 }
 
-fn update_bullet_shot(
-    mut query_player: Query<&mut Transform, (With<Player>, Without<Bullet>)>,
-    mut query_bullets: Query<
-        (&mut Transform, &ViewVisibility, &mut Visibility),
-        (With<Player>, With<Bullet>),
-    >,
-    mut timer: ResMut<ShotTimer>,
+fn update_bullet_spawn(
+    mut query_bullets: Query<(&mut Transform, &ViewVisibility, &mut Visibility), With<BulletData>>,
+    mut query_player: Query<(&Transform, &mut PlayerData), Without<BulletData>>,
     time: Res<Time>,
 ) {
-    if timer.0.tick(time.delta()).just_finished() {
+    let (player_transform, mut player) = query_player.single_mut();
+
+    if player.shot_timer.tick(time.delta()).just_finished() {
         query_bullets
             .iter_mut()
             .find(|(_, is_visible, _)| !is_visible.get())
             .map(|(mut transform, _, mut visibility)| {
-                transform.translation = query_player.single_mut().translation.clone();
+                transform.translation = player_transform.translation;
                 *visibility = Visibility::Visible;
             });
     }
 }
 
-fn update_bullets_position(mut query: Query<(&Bullet, &mut Transform, &ViewVisibility)>) {
+fn update_bullets_position(
+    mut query: Query<(&BulletData, &mut Transform, &ViewVisibility)>,
+) {
     for (bullet, mut transform, visibility) in &mut query {
         if visibility.get() {
             transform.translation.y += bullet.speed;
@@ -108,12 +144,11 @@ fn update_bullets_position(mut query: Query<(&Bullet, &mut Transform, &ViewVisib
 }
 
 fn update_enemy_position(
-    mut query: Query<(&mut Enemy, &mut Transform)>,
-    mut timer: ResMut<EnemyTimer>,
+    mut query_enemy: Query<(&mut EnemyData, &mut Transform)>,
     time: Res<Time>,
 ) {
-    for (mut enemy, mut transform) in &mut query {
-        if timer.0.tick(time.delta()).just_finished() {
+    for (mut enemy, mut transform) in &mut query_enemy {
+        if enemy.move_timer.tick(time.delta()).just_finished() {
             enemy.velocities.rotate_left(1);
         }
         let vel = enemy.velocities.first().unwrap_or(&0.);
@@ -128,12 +163,13 @@ fn escape_game(mut exit: EventWriter<AppExit>, keys: Res<ButtonInput<KeyCode>>) 
 }
 
 fn change_colors(
-    mut enemies: Query<&mut Sprite, With<Enemy>>,
-    mut bullets: Query<&mut Sprite, (With<Bullet>, Without<Enemy>)>,
+    mut enemy_sprites: Query<&mut Sprite, (With<EnemyData>, Without<BulletData>)>,
+    mut bullets: Query<&mut Sprite, With<BulletData>>,
 ) {
-    enemies
+    enemy_sprites
         .iter_mut()
         .for_each(|mut sprite| sprite.color = Color::BLACK);
+
     bullets
         .iter_mut()
         .for_each(|mut sprite| sprite.color = Color::RED);
@@ -142,15 +178,13 @@ fn change_colors(
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ShotTimer(Timer::from_seconds(0.1, TimerMode::Repeating)))
-        .insert_resource(EnemyTimer(Timer::from_seconds(1.5, TimerMode::Repeating)))
         .add_systems(Startup, startup)
         .add_systems(PostStartup, change_colors)
         .add_systems(PreUpdate, (update_player_position, escape_game))
         .add_systems(
             Update,
             (
-                update_bullet_shot,
+                update_bullet_spawn,
                 update_bullets_position,
                 update_enemy_position,
             ),
