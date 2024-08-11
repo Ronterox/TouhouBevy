370a251353c334405bf61301f772bbd9b192dbe2
Ricardo <ricarditomontserrat@gmail.com>
2024-08-11 22:40:41 +0200

    Gunner bundle for composition

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,26 @@ use bevy::{
 };
 
 #[derive(Component)]
-struct PlayerData {
+struct Player {
     speed: f32,
 }
 
-#[derive(Component, Clone)]
-struct EnemyData {
+#[derive(Component)]
+struct Enemy {
     speed: f32,
     directions: Vec<f32>,
     move_timer: Timer,
 }
 
 #[derive(Component, Clone)]
-struct BulletData {
+struct Bullet {
     tag: Tag,
     direction: Vec3,
     speed: f32,
     damage: u32,
 }
 
-#[derive(Component, Clone)]
+#[derive(Component)]
 struct Gunner {
     tag: Tag,
     shot_timer: Timer,
@@ -34,9 +34,16 @@ struct Gunner {
 struct ChangeColor(Color);
 
 #[derive(Bundle, Clone)]
-struct BodyData<T: Component> {
+struct EntityBundle<T: Component> {
     data: T,
     sprite: SpriteBundle,
+}
+
+#[derive(Bundle)]
+struct GunnerBundle<T: Component> {
+    gunner: Gunner,
+    health_bar: HealthBar,
+    entity: EntityBundle<T>,
 }
 
 #[derive(Component)]
@@ -54,56 +61,87 @@ enum Tag {
     Enemy,
 }
 
-type Player = BodyData<PlayerData>;
-type Enemy = BodyData<EnemyData>;
-type Bullet = BodyData<BulletData>;
+type PlayerBundle = GunnerBundle<Player>;
+type EnemyBundle = GunnerBundle<Enemy>;
+type BulletBundle = EntityBundle<Bullet>;
 
 impl Gunner {
+    fn new(tag: Tag, delay: f32) -> Self {
+        Self {
+            tag,
+            shot_timer: Timer::from_seconds(delay, TimerMode::Repeating),
+        }
+    }
+
     fn can_shoot(&mut self, delta: std::time::Duration) -> bool {
         self.shot_timer.tick(delta).just_finished()
     }
 }
 
-impl Player {
-    fn new(texture: &Handle<Image>) -> Self {
+impl PlayerBundle {
+    fn new(texture: Handle<Image>) -> Self {
         Self {
-            data: PlayerData { speed: 10.0 },
-            sprite: SpriteBundle {
-                transform: Transform::from_xyz(0., -200., 0.).with_scale(Vec3::splat(0.12)),
-                texture: texture.clone(),
-                ..default()
+            entity: EntityBundle {
+                data: Player { speed: 10.0 },
+                sprite: SpriteBundle {
+                    transform: Transform::from_xyz(0., -200., 0.).with_scale(Vec3::splat(0.12)),
+                    texture,
+                    ..default()
+                },
+            },
+            gunner: Gunner::new(Tag::Player, 0.1),
+            health_bar: HealthBar {
+                tag: Tag::Player,
+                health: 1,
+                on_death: || todo!("Player died, you lose!"),
+                on_hit: |health| println!("Player health: {}", health),
+                hitbox_size: 25.,
             },
         }
     }
 }
 
-impl Enemy {
-    fn new(texture: &Handle<Image>) -> Self {
+impl EnemyBundle {
+    fn new(texture: Handle<Image>) -> Self {
         Self {
-            data: EnemyData {
-                speed: 5.,
-                directions: vec![-1., 0., 1., 0., 1., 0., -1., 0.],
-                move_timer: Timer::from_seconds(1.5, TimerMode::Repeating),
+            entity: EntityBundle {
+                data: Enemy {
+                    speed: 5.,
+                    directions: vec![-1., 0., 1., 0., 1., 0., -1., 0.],
+                    move_timer: Timer::from_seconds(1.5, TimerMode::Repeating),
+                },
+                sprite: SpriteBundle {
+                    transform: Transform::from_xyz(0., 200., 0.).with_scale(Vec3::splat(0.2)),
+                    texture,
+                    ..default()
+                },
             },
-            sprite: SpriteBundle {
-                transform: Transform::from_xyz(0., 200., 0.).with_scale(Vec3::splat(0.2)),
-                texture: texture.clone(),
-                ..default()
+            gunner: Gunner::new(Tag::Enemy, 0.5),
+            health_bar: HealthBar {
+                tag: Tag::Enemy,
+                health: 200,
+                on_death: || todo!("Enemy died, you win!"),
+                on_hit: |health| {
+                    if health % 5 == 0 {
+                        println!("Enemy health: {}", health);
+                    }
+                },
+                hitbox_size: 100.,
             },
         }
     }
 }
 
-impl Bullet {
+impl BulletBundle {
     fn new(
         tag: Tag,
-        texture: &Handle<Image>,
+        texture: Handle<Image>,
         direction: Vec3,
         speed: f32,
         size_percentage: f32,
     ) -> Self {
         Self {
-            data: BulletData {
+            data: Bullet {
                 tag,
                 speed,
                 direction,
@@ -112,9 +150,19 @@ impl Bullet {
             sprite: SpriteBundle {
                 transform: Transform::from_xyz(0., 0., 0.).with_scale(Vec3::splat(size_percentage)),
                 visibility: Visibility::Hidden,
-                texture: texture.clone(),
+                texture,
                 ..default()
             },
+        }
+    }
+}
+
+impl HealthBar {
+    fn take_damage(&mut self, damage: u32) {
+        self.health -= damage;
+        (self.on_hit)(self.health);
+        if self.health == 0 {
+            (self.on_death)();
         }
     }
 }
@@ -136,61 +184,33 @@ fn startup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn((
-        Player::new(&asset_server.load(PLAYER_IMG_PATH)),
-        Gunner {
-            tag: Tag::Player,
-            shot_timer: Timer::from_seconds(0.1, TimerMode::Repeating),
-        },
-        HealthBar {
-            tag: Tag::Player,
-            health: 1,
-            on_death: || todo!("Player died, you lose!"),
-            on_hit: |health| println!("Player health: {}", health),
-            hitbox_size: 25.,
-        },
-    ));
+    commands
+        .spawn((PlayerBundle::new(asset_server.load(PLAYER_IMG_PATH)),))
+        .with_children(|parent| {
+            parent.spawn(MaterialMesh2dBundle {
+                mesh: Mesh2dHandle(meshes.add(Circle { radius: 50. })),
+                material: materials.add(Color::rgba(1., 0., 0., 0.8)),
+                transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+                ..default()
+            });
+        });
 
     commands.spawn((
-        Enemy::new(&asset_server.load(ENEMY_IMG_PATH)),
-        Gunner {
-            tag: Tag::Enemy,
-            shot_timer: Timer::from_seconds(0.5, TimerMode::Repeating),
-        },
+        EnemyBundle::new(asset_server.load(ENEMY_IMG_PATH)),
         ChangeColor(Color::BLACK),
-        HealthBar {
-            tag: Tag::Enemy,
-            health: 200,
-            on_death: || todo!("Enemy died, you win!"),
-            on_hit: |health| {
-                if health % 5 == 0 {
-                    println!("Enemy health: {}", health);
-                }
-            },
-            hitbox_size: 100.,
-        },
     ));
-
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(meshes.add(Circle { radius: 10. })),
-        material: materials.add(Color::rgba(1., 0., 0., 0.5)),
-        transform: Transform::from_xyz(0., -200., 0.),
-        ..default()
-    });
 
     let bullet_texture = asset_server.load(BULLET_IMG_PATH);
-    let color = ChangeColor(Color::RED);
 
-    let bullet = Bullet::new(Tag::Player, &bullet_texture, DIR_UP, 20., 0.05);
-    commands.spawn_batch(std::iter::repeat((bullet, color.clone())).take(5));
+    let bullet = BulletBundle::new(Tag::Player, bullet_texture.clone(), DIR_UP, 20., 0.05);
+    commands.spawn_batch(std::iter::repeat((bullet, ChangeColor(Color::GRAY))).take(5));
 
-    let bullet = Bullet::new(Tag::Enemy, &bullet_texture, DIR_DOWN, 2., 0.1);
-    commands.spawn_batch(std::iter::repeat((bullet, color)).take(5));
+    let bullet = BulletBundle::new(Tag::Enemy, bullet_texture, DIR_DOWN, 2., 0.1);
+    commands.spawn_batch(std::iter::repeat((bullet, ChangeColor(Color::RED))).take(5));
 }
 
 fn update_player_position(
-    mut player: Query<(&mut Transform, &PlayerData)>,
-    mut hitbox: Query<&mut Transform, (With<Mesh2dHandle>, Without<PlayerData>)>,
+    mut player: Query<(&mut Transform, &Player)>,
     keys: Res<ButtonInput<KeyCode>>,
 ) {
     let (mut ply_transform, ply_data) = player.single_mut();
@@ -198,7 +218,6 @@ fn update_player_position(
     let mut if_press_move = |key: KeyCode, dir: Vec3| {
         if keys.pressed(key) {
             ply_transform.translation += dir * ply_data.speed;
-            hitbox.single_mut().translation = ply_transform.translation;
         }
     };
 
@@ -209,13 +228,8 @@ fn update_player_position(
 }
 
 fn update_bullets_spawn(
-    mut bullets: Query<(
-        &mut Transform,
-        &ViewVisibility,
-        &mut Visibility,
-        &BulletData,
-    )>,
-    mut gunners: Query<(&Transform, &mut Gunner), Without<BulletData>>,
+    mut bullets: Query<(&mut Transform, &ViewVisibility, &mut Visibility, &Bullet)>,
+    mut gunners: Query<(&Transform, &mut Gunner), Without<Bullet>>,
     time: Res<Time>,
 ) {
     for (gunner_transform, mut gunner) in &mut gunners {
@@ -233,7 +247,7 @@ fn update_bullets_spawn(
     }
 }
 
-fn update_bullets_position(mut bullets: Query<(&BulletData, &mut Transform, &ViewVisibility)>) {
+fn update_bullets_position(mut bullets: Query<(&Bullet, &mut Transform, &ViewVisibility)>) {
     bullets
         .iter_mut()
         .filter(|(_, _, visibility)| visibility.get())
@@ -242,7 +256,7 @@ fn update_bullets_position(mut bullets: Query<(&BulletData, &mut Transform, &Vie
         });
 }
 
-fn update_ai_position(mut enemies: Query<(&mut EnemyData, &mut Transform)>, time: Res<Time>) {
+fn update_ai_position(mut enemies: Query<(&mut Enemy, &mut Transform)>, time: Res<Time>) {
     for (mut enemy, mut transform) in &mut enemies {
         if enemy.move_timer.tick(time.delta()).just_finished() {
             enemy.directions.rotate_left(1);
@@ -264,9 +278,9 @@ fn change_colors(mut sprites: Query<(&mut Sprite, &ChangeColor)>) {
         .for_each(|(mut sprite, ChangeColor(color))| sprite.color = *color);
 }
 
-fn update_entity_hit(
+fn update_entities_hits(
     mut entities: Query<(&Transform, &mut HealthBar)>,
-    mut bullets: Query<(&Transform, &BulletData, &mut Visibility)>,
+    mut bullets: Query<(&Transform, &Bullet, &mut Visibility)>,
 ) {
     bullets
         .iter_mut()
@@ -277,11 +291,7 @@ fn update_entity_hit(
                 .filter(|(_, entity)| bullet.tag != entity.tag)
                 .for_each(|(transform, mut entity)| {
                     if bullet_pos.translation.distance(transform.translation) < entity.hitbox_size {
-                        entity.health -= bullet.damage;
-                        (entity.on_hit)(entity.health);
-                        if entity.health == 0 {
-                            (entity.on_death)()
-                        }
+                        entity.take_damage(bullet.damage);
                         *bullet_visibility = Visibility::Hidden;
                     }
                 });
@@ -302,6 +312,6 @@ fn main() {
                 update_ai_position,
             ),
         )
-        .add_systems(PostUpdate, update_entity_hit)
+        .add_systems(PostUpdate, update_entities_hits)
         .run();
 }
